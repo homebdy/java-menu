6cd6dee79cdab847d8bdd09d8fa63581a00949bd
김연진 <love_homebody@naver.com>
2023-08-24 01:07:17 +0900

    feat(InputView): 코치 이름 입력

--- src/main/java/menu/controller/MenuController.java
+++ src/main/java/menu/controller/MenuController.java
@@ -1,13 +1,16 @@
 package menu.controller;
 
+import menu.view.InputView;
 import menu.view.OutputView;
 
 public class MenuController {
 
     private final OutputView outputView = new OutputView();
+    private final InputView inputView = new InputView();
 
     public void start() {
         outputView.printStartMessage();
         outputView.printNameMessage();
+        inputView.readNames();
     }
 }
