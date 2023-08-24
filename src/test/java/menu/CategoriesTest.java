ff7b3565894a9b1c8c39e2f6a90cc97d9ceded5a
김연진 <love_homebody@naver.com>
2023-08-25 02:37:01 +0900

    test(categories): 카테고리 추천 테스트

---
+++ src/test/java/menu/CategoriesTest.java
@@ -0,0 +1,23 @@
+package menu;
+
+import menu.domain.Categories;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class CategoriesTest {
+
+    @Test
+    void recommendCategories() {
+        Categories categories = new Categories();
+
+        assertThat(categories.getCategories().size()).isEqualTo(5);
+    }
+
+    @Test
+    void CategoriesToString() {
+        Categories categories = new Categories();
+
+        assertThat(categories.toString()).contains("[ 카테고리 |");
+    }
+}
