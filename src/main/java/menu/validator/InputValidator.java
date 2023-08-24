499da34cde71f1a0596c394b6ec9d50ed7316bf7
김연진 <love_homebody@naver.com>
2023-08-25 02:59:14 +0900

    fix(InputValidator): 빈 값 입력시 오류 발생 수정

--- src/main/java/menu/validator/InputValidator.java
+++ src/main/java/menu/validator/InputValidator.java
@@ -6,7 +6,7 @@ import java.util.regex.Pattern;
 
 public class InputValidator {
 
-    private static final String DELIMITER_REGEXP = "(\\S|,)+(\\S)?";
+    private static final String DELIMITER_REGEXP = "(\\S|)+(,\\S)?";
 
     public void validateDelimiter(String input) {
         if (!Pattern.matches(DELIMITER_REGEXP, input)) {
