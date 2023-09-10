f95d828d66a94d1c82c1327244de1249ae811ba1
김연진 <love_homebody@naver.com>
2023-09-10 20:17:30 +0900

    feat(ExcludedMenu): 못먹는 음식 2개 이상 입력 시 예외 발생

--- src/main/java/menu/constant/ExceptionMessage.java
+++ src/main/java/menu/constant/ExceptionMessage.java
@@ -5,7 +5,8 @@ public enum ExceptionMessage {
     INCORRECT_DELIMITER("','로 구분하여 입력해야합니다."),
     INCORRECT_NAME_RANGE("코치의 이름은 2~4자까지 입력할 수 있습니다."),
     INCORRECT_COACH_NUMBER("코치는 2명 이상, 5명 이하만 입력할 수 있습니다."),
-    INCORRECT_RECOMMEND("등록되지 않은 카테고리입니다.");
+    INCORRECT_RECOMMEND("등록되지 않은 카테고리입니다."),
+    EXCLUDED_FOOD_SIZE("못먹는 음식은 0~2개까지 입력가능합니다.");
 
     private static final String PREFIX = "[ERROR] ";
     private final String message;
--- src/main/java/menu/domain/ExcludedMenu.java
+++ src/main/java/menu/domain/ExcludedMenu.java
@@ -1,14 +1,25 @@
 package menu.domain;
 
+import menu.constant.ExceptionMessage;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class ExcludedMenu {
 
+    private static final int MAX_SIZE = 2;
+
     private final List<String> elements = new ArrayList<>();
 
     public void addElements(List<String> menus) {
+        validateSize(menus);
         elements.addAll(menus);
+    }
+
+    private void validateSize(List<String> menus) {
+        if (!(menus.size() <= MAX_SIZE)) {
+            throw new IllegalArgumentException(ExceptionMessage.EXCLUDED_FOOD_SIZE.toString());
+        }
     }
 
     public boolean canEat(String menu) {
