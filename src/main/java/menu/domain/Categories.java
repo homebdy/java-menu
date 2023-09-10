3c816eb9e6ce88f70409e2d65e135bc4f57f7ebb
김연진 <love_homebody@naver.com>
2023-09-10 18:53:31 +0900

    feat(Categories): 이미 2번 추천된 카테고리일 경우 재추천

--- src/main/java/menu/domain/Categories.java
+++ src/main/java/menu/domain/Categories.java
@@ -7,17 +7,38 @@ import java.util.List;
 
 public class Categories {
 
+    private static final int RECOMMEND_COUNT = 5;
+    private static final int MAX_RECOMMEND_COUNT = 2;
+    private static final int START_RANDOM_NUMBER = 1;
+    private static final int END_RANDOM_NUMBER = 5;
+
     private final List<Category> elements;
 
     public Categories() {
         this.elements = recommendCategory();
+        System.out.print(elements);
     }
 
     private List<Category> recommendCategory() {
         List<Category> categories = new ArrayList<>();
-        for (int i = 0; i < 5; i++) {
-            categories.add(Category.getRecommendFood(Randoms.pickNumberInRange(1, 5)));
+        while (categories.size() < RECOMMEND_COUNT) {
+            Category category = randomRecommend();
+            if (isDuplicateRecommend(categories, category)) {
+                continue;
+            }
+            categories.add(category);
         }
         return categories;
+    }
+
+    private Category randomRecommend() {
+        int random = Randoms.pickNumberInRange(START_RANDOM_NUMBER, END_RANDOM_NUMBER);
+        return Category.getRecommendFood(random);
+    }
+
+    private boolean isDuplicateRecommend(List<Category> categories, Category recommendCategory) {
+        return categories.stream()
+                .filter(category -> category == recommendCategory)
+                .count() >= MAX_RECOMMEND_COUNT;
     }
 }
