433f4ef0ef9c394a5317af67dfa618837341cec2
김연진 <love_homebody@naver.com>
2023-09-10 17:08:41 +0900

    feat(OutputView): 추천 시작 안내 메시지 출력 기능 구현

---
+++ src/main/java/menu/controller/MenuController.java
@@ -0,0 +1,13 @@
+package menu.controller;
+
+import menu.view.OutputView;
+
+public class MenuController {
+
+    private final OutputView outputView = new OutputView();
+
+    public void start() {
+        outputView.printStart();
+    }
+
+}
---
+++ src/main/java/menu/view/OutputView.java
@@ -0,0 +1,10 @@
+package menu.view;
+
+import menu.constant.OutputMessage;
+
+public class OutputView {
+
+    public void printStart() {
+        System.out.println(OutputMessage.START_RECOMMEND.getMessage());
+    }
+}
