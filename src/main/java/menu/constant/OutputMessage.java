3f8587b778a1f747293c509110a8647cdc2eb6ea
김연진 <love_homebody@naver.com>
2023-12-12 14:25:37 +0900

    feat(OutputView): 코치 이름 입력 문구 출력

--- src/main/java/menu/constant/OutputMessage.java
+++ src/main/java/menu/constant/OutputMessage.java
@@ -2,7 +2,8 @@ package menu.constant;
 
 public enum OutputMessage {
 
-    START_RECOMMEND("점심 메뉴 추천을 시작합니다.");
+    START_RECOMMEND("점심 메뉴 추천을 시작합니다."),
+    READ_COACH_NAME("코치의 이름을 입력해 주세요. (, 로 구분)");
 
     private final String message;
 
--- src/main/java/menu/view/OutputView.java
+++ src/main/java/menu/view/OutputView.java
@@ -7,4 +7,8 @@ public class OutputView {
     public void printStartService() {
         System.out.println(OutputMessage.START_RECOMMEND.getMessage());
     }
+
+    public void printReadNameMessage() {
+        System.out.println(OutputMessage.READ_COACH_NAME.getMessage());
+    }
 }
