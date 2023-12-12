0b164fe8edc1dd70836d90e1a58cebd52a503600
김연진 <love_homebody@naver.com>
2023-12-12 14:23:00 +0900

    feat(OutputView): 점심 메뉴 추천 시작 문구 출력

---
+++ src/main/java/menu/view/OutputView.java
@@ -0,0 +1,10 @@
+package menu.view;
+
+import menu.constant.OutputMessage;
+
+public class OutputView {
+
+    public void printStartService() {
+        System.out.println(OutputMessage.START_RECOMMEND.getMessage());
+    }
+}
